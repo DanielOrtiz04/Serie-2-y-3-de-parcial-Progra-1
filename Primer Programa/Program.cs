41256f8e3f18b8b565c05772096316b25d20f073
Daniel Ortiz <dortixc13@miumg.edu.gt>
2024-03-09 12:25:16 -0600

    Agregar archivos de proyecto.

---
+++ Primer Programa/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primer_Programa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Calcular 3 productos
+            Console.Write("Bienvenido");
+            Console.WriteLine("Ingrese su nombre:");
+            string nombreUsuario = Console.ReadLine();
+
+            double[] ingresos = new double[3];
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write($"Ingrese primer producto {i + 1}: ");
+                ingresos[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            CalcularSuma(ingresos, nombreUsuario);
+        }
+
+        static void CalcularSuma(double[] ingresos, string nombreUsuario)
+        {
+            double sumaIngresos = 0;
+            foreach (var ingreso in ingresos)
+            {
+                sumaIngresos += ingreso;
+            }
+
+            double Ingresos = sumaIngresos / ingresos.Length;
+
+            Console.WriteLine($" El Total es:  {sumaIngresos} ");
+            Console.ReadLine();
+        }
+    }
+}
+       
---
+++ Proyecto 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primer_Programa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            Console.Write("Bienvenido ");
+            Console.Write("Dime un nombre y apellido: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine(nombre + "@gmail.com");
+            Console.ReadLine();
+        }
+    }
+}
